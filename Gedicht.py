16ded0b3ad0de5eb0d13306b3ddb9bbc426da1dd
Kilian Rijks <coolkil321@gmail.com>
2020-09-10 14:57:01 +0200

    Gedicht.py is toegevoegd

---
+++ Gedicht.py
@@ -0,0 +1,13 @@
+vers1 = "Mijn naam is Kilian Rijks."
+vers2 = "Ik zit nu op het ma maar dat is altijd wel een lange reis."
+vers3 = "Ik game heel veel en dat is waarom ik me niet erg vaak verveel."
+vers4 = "Misschien vraag je je dan af wat ik dan speel."
+vers5 = "Op het moment speel ik veel among us."
+vers6 = "Ik weet niet of u het kent maar het is nu best wel bekend."
+vers7 = "Het is een game over pain and betray en dat is waarom mijn vriend verdween ):."
+vers8 = "Weetje wat ik vind dit wel genoeg informatie among us geeft me al genoeg pijn en frustratie."
+vers9 = "Maar dit is waar ik stop want me inspiratie is op."
+ 
+ 
+ 
+
