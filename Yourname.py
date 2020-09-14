4110e64d752c475861102204e0be5b75bae14f24
Kilian Rijks <coolkil321@gmail.com>
2020-09-14 10:54:43 +0200

    Changed Yourname.py and Stats.py

--- Yourname.py
+++ Yourname.py
@@ -1,6 +1,31 @@
-print("Hello You!, ik ben Kilian")
-name = input("Wie ben jij?: ")
-print("Hello " + name)
+while True:
+  print("Hello You!, ik ben Kilian")
+  print("de datum en tijd is")
+  import datetime
+  x = datetime.datetime.now()
+  print(x)
+  name = input("Wie ben jij?: ")
+  print("Hello " + name)
+  print("Wil je dit programma nog een keer doen? Type y/n")
+  answer = input()
+
+  if answer == 'n':
+    print("Dankjewel")
+    break
+
+  if answer == 'y':
+    print("Restarting...")
+  
+
+
+
+  
+
+ 
+
+
+  
+
 
 
 
