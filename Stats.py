c7dfd019a79645e5ad4dc82166bbc60c5d201001
Kilian Rijks <coolkil321@gmail.com>
2020-09-23 14:11:13 +0200

    Changed Stats.py

--- Stats.py
+++ Stats.py
@@ -1,24 +1,25 @@
 Username = "NineVision"
-Hp = '27'
-Mana = '367'
-Strength =  '57'
+Hp = 27
+Mana = 367
+Strength =  57
 Items = "tougher times, soldier syringe, sticky bombs, tesla coil, brilliant behemoth"
-Server = '15'
-Speed = '249'
-Level = '9001'
-Sanity = '100'
-Stamina = '999999999999'
+Server = 15
+Speed = 249
+Level = 9001
+Sanity = 100
+Stamina = 999999999999
 Inwater = True
 
 print("Username: " + Username)
-print("Hp: " + Hp)
-print("Strength: " + Strength)
+print("Hp: ",Hp)
+print("Mana: ",Mana)
+print("Strength: ",Strength)
 print("Items: " + Items)
-print("Players in server: " + Server)
-print("Speed: " + Speed)
-print("Level: " + Level)
-print("Sanity: " + Sanity)
-print("Stamina: " + Stamina)
+print("Players in server: ",Server)
+print("Speed: ",Speed)
+print("Level: ",Level)
+print("Sanity: ",Sanity)
+print("Stamina: ",Stamina)
 print("Inwater")
 print(Inwater)
 
