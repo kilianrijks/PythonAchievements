8c27143d2b3b76d581c1d9f00c19f286b641df6e
Kilian Rijks <coolkil321@gmail.com>
2020-09-30 15:24:01 +0200

    Added choice.py

---
+++ Choice.py
@@ -0,0 +1,88 @@
+while True:
+
+  print("Je word wakker om 7 uur en je wekker gaat af. Wat doe je?")
+  print("a. Zet je wekker op snooze")
+  print("b. Wake up!")
+
+
+  Antwoord1 = input("Antwoord met a of b: ")
+
+
+  if Antwoord1 == 'a':
+    print("Je bent een lui persoon")
+
+  if Antwoord1 == 'b':
+    print("Good choice!")
+
+  print("Oke wat ga je nu doen?")
+  print("a. Ga ontbijten")
+  print("b. Ga terug naar bed of blijf slapen")
+
+  Antwoord2 = input("Antwoord met a of b: ")
+
+  if Antwoord2 == 'a':
+    print("Je loopt naar beneden om te ontbijten")
+
+  if Antwoord2 == 'b'and Antwoord1 == 'b':
+    print("Een beetje lui om weer terug naar je bed te gaan maar oke")
+
+  if Antwoord2 == 'b' and Antwoord1 == 'a':
+    print("You are joking right?!")
+
+  print("Je gaat nu ontbijten en als je ervoor gekozen had om lui te zijn word je nu door de code geforced om te ontbijten. De vraag is wat wil je ontbijten?")
+  print("a. Cornflakes")
+  print("b. Brood")
+  print("c. yogurt")
+
+  Antwoord3 = input("Antwoord met a of b: ")
+
+  if Antwoord3 == 'a':
+    print("Je besluit om cornflakes te eten")
+
+  if Antwoord3 == 'b':
+    print("Je besluit om Brood te eten")
+
+  if Antwoord3 == 'c':
+    print("Je besluit om yogurt te eten")
+
+  print("Na het ontbijt ga je naar school maar ga je met OV of met de fiets?")
+  print("a. OV")
+  print("b. Fiets")
+
+  Antwoord4 = input("Antwoord met a of b: ")
+
+  if Antwoord4 == 'b':
+    print("Oke je gaat met de fiets")
+
+  if Antwoord4 == 'a':
+    print("Oke je gaat met het OV")
+
+  print("Je bent op het station, ga je nu lopend naar school of met de bus?")
+  print("a. Lopend")
+  print("b. Met de bus")
+
+  Antwoord5 = input("Antwoord met a of b: ")
+
+  if Antwoord5 == 'a':
+    print("Je gaat lopend en je struikelt over een steen, hierdoor kom je te laat op school")
+
+  if Antwoord5 == 'b':
+    print("Je gaat met de bus naar school en je komt ruim optijd")
+
+  
+  
+
+
+
+
+
+
+
+
+      
+
+
+
+  
+  
+      
