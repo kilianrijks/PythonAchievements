5c9dc284329adf2ef055ab54f541fcfb262dacb1
Kilian Rijks <coolkil321@gmail.com>
2020-09-30 14:35:42 +0200

    Added some new files

---
+++ Stringslicing.py
@@ -0,0 +1,20 @@
+naam = "Kilian Rijks"
+voornaam = naam[0:6]
+print(voornaam)
+
+tekst1 = "De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde"
+tekst01 = tekst1[3:9]
+print(tekst01)
+
+tekst2 = "SD vakken zijn Python, UXD, Frontend development en Backend development en nog veel meer ..."
+tekst02 = tekst2[23:71]
+print(tekst02)
+
+tekst3 = "Wat is hier het laatste woord?"
+tekst03 = tekst3[24:31] 
+print(tekst03)
+
+tekst4 = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+tekst04 = tekst4[4:8]
+tekst004 = tekst4[28:33]
+print(tekst04 + tekst004)
---
+++ Variableswetrust.py
@@ -0,0 +1,25 @@
+Character1 = '@'
+Float1 = 5.2
+Boolean1 = True
+Boolean2 = Boolean1
+Integer1 = 5
+Integer2 = 2
+Integer3 = Integer1 - Integer2
+String1 = "Dit is een tekst."
+String2 = "Dit is ook een tekst."
+String3 = String1 + " " + String2
+
+print(Character1)
+print(Float1)
+print(Boolean2)
+print(Integer3)
+print(String3)
+
+
+ 
+
+
+
+
+
+
